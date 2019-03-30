b4e355848c9c448b20c813763fd440d79f6a90aa
yamila rangel <yamilarangel.nyc@gmail.com>
2019-03-30 12:29:53 -0400

    ngcgh

---
+++ script.js
@@ -0,0 +1,56 @@
+
+$(document).ready(function(){
+
+	$("#icon").hover(enter,leave);
+
+	function enter(){
+
+		this.src = "https:media.giphy.com/media/xULW8jLxnHL9wcYsko/giphy.gif";
+
+	}	
+
+	function leave(){
+
+		this.src = "https://pbs.twimg.com/media/DDBnHYrV0AAXO8u.jpg";
+
+	}
+
+});
+
+
+
+$(document).ready(function(){
+
+	$("#building").hover(enter,leave);
+
+	function enter(){
+
+		this.src = "https://media.giphy.com/media/28Nev3k184A9PdSDCg/giphy.gif";
+
+	}	
+
+	function leave(){
+
+		this.src = "https://farm1.staticflickr.com/2860/34153105092_8b2360a709.jpg";
+
+	}
+
+});
+
+$(document).ready(function(){
+
+	$("#logo").hover(enter,leave);
+
+	function enter(){
+
+		this.src = "https://media.giphy.com/media/d14TB7oo54AA8/giphy.gif";
+
+	}	
+
+	function leave(){
+
+		this.src = "https://www.studentleadershipnetwork.org/wp-content/uploads/2018/08/TYWLS_Seal_Color_Pos_PNG.png";
+
+	}
+
+});
